c2f6d0f32e524c5d464ca211050b5ca2f17591e4
Dominik Gasparic <56818232+codedoga@users.noreply.github.com>
2021-11-28 00:27:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
-{
-    p = document.getElementById("principal").value;
+{   // input variables
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // calculated end year and amount
+    var year = new Date().getFullYear() + parseInt(years);
+    var amount = principal * years * rate /100;
     
+    // validation
+    if (principal <= 0 || principal == NaN){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    } else {
+        document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,<br\>at an interest rate of <mark>"+rate+"%</mark>.<br\>You will receive an amount of <mark>"+amount+"</mark>,<br\>in the year <mark>"+year+"</mark><br\>"
+    }    
+}
+
+//display rate % with slider change
+function updateRate()
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
 }
         
